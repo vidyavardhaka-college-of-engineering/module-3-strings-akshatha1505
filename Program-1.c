a80463afb1b49def31930060b2cd8322dd0e3bc5
akshatha1505 <84765349+akshatha1505@users.noreply.github.com>
2021-07-05 08:22:33 +0530

    done

--- Program-1.c
+++ Program-1.c
@@ -11,9 +11,9 @@ Complete the given program with string declaration/initialization & read/print t
 #include <stdio.h>
 int main() 
 {
-                      //Declare the string
-printf("Enter a String: ")
-                      //read the string from user
-                      //Print the string
+char str[50];                   
+printf("Enter a String: ");
+gets(str);                      
+printf("The input string is %s",str);                      
 return 0;
 }
